bb2efb6ac19235101b81e5bba36493de3b14f233
heyvanak <alimahabseo@gmail.com>
2025-05-26 11:46:00 +0330

    Update bot.py

--- bot.py
+++ bot.py
@@ -1,12 +1,13 @@
 from telegram.ext import ApplicationBuilder
 from handlers.start import start_handler
-from config import BOT_TOKEN
 from handlers.menu import menu_handler
-
+from handlers.ads import ads_handler
+from config import BOT_TOKEN
 
 app = ApplicationBuilder().token(BOT_TOKEN).build()
 
 app.add_handler(start_handler)
 app.add_handler(menu_handler)
+app.add_handler(ads_handler)
 
 app.run_polling()
